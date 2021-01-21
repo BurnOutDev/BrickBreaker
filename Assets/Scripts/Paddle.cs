0678f2c7a068b59e57ac70024feb0208ea276fd9
Irakli Murusidze <irakli.dev@synvestment.co>
2021-01-21 15:28:41 +0400

    added ball-paddle collision and corrected paddle movement

--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -25,10 +25,30 @@ public class Paddle : MonoBehaviour
     {
         float h = Input.GetAxisRaw("Horizontal");
 
-        if ((int)h == 0 && rb.velocity == Vector3.zero)
+        if ((int)h == 0 && rb.velocity != Vector3.zero)
         {
             rb.velocity = Vector3.zero;
         }
         rb.MovePosition(transform.position + new Vector3(h, 0, 0).normalized * speed * Time.fixedDeltaTime);
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ball"))
+        {
+            Rigidbody ballRb = collision.gameObject.GetComponent<Rigidbody>();
+            float vel = Ball.initialForce;
+            Vector3 hitPoint = collision.contacts[0].point;
+            float difference = transform.position.x - hitPoint.x;
+
+            if (hitPoint.x < transform.position.x)
+            {
+                ballRb.AddForce(new Vector3(-(Mathf.Abs(difference * 200)), vel, 0));
+            }
+            else
+            {
+                ballRb.AddForce(new Vector3(Mathf.Abs(difference * 200), vel, 0));
+            }
+        }
     }
 }
